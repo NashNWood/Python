3898f3e300ff9067ed89f9ee860d6a0f78f36281
Nash Noble Wood <nashua.wood@gmail.com>
2017-03-25 10:06:40 -0700

    range() function exercises
    This project demonstrates several features of the range() function.

---
+++ Exercise46.py
@@ -0,0 +1,25 @@
+# Python:   3.6.1
+#
+# Author:   Nash N. Wood
+#
+#Purpose:   The Tech Acadmey - Python Course, Item 46
+#           Python range() function
+#           Demonstrate usage of the range() function.
+
+
+
+
+
+my_list = [0,1,2,3]
+for i in range(4):
+    print(my_list[i])
+    
+
+for i in range(3,-1,-1):
+    print (my_list[i])
+
+
+my_list2 = [0,1,2,3,4,5,6,7,8]
+for i in range(8,1,-2):
+    print (my_list2[i])
+        
